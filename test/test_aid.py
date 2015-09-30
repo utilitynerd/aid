407cc29f55f61f011c711fceb69eb8403ae8f0e7
Mike Jones <mikejones@berkeley.edu>
2015-09-30 10:34:39 -0700

    remove extra imports lowercase function name

--- test/test_aid.py
+++ test/test_aid.py
@@ -1,7 +1,4 @@
-from aid.iptables import  *
 from aid.aid import *
-import pytest
-import requests
 import collections
 
 
@@ -9,7 +6,8 @@ def test_no_duplicate_ips():
     c = collections.Counter(get_aidlist_ips())
     assert set(c.values()) == {1}
 
-def test_AidEntry_captures_all_attributes():
+
+def test_aidentry_captures_all_attributes():
     config = load_config()
     # get a single entry as an example.  call_sock_api returns a list of dicts
     entry = call_sock_api(config, 'aggressive_ips')['aggressive_ips'][0]
