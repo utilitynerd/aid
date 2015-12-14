41b3c3b1535162b4f163483b0c80884b9b2bc590
Mike Jones <mikejones@berkeley.edu>
2015-12-14 10:48:43 -0800

    updating after function name change

--- test/test_aid.py
+++ test/test_aid.py
@@ -9,7 +9,7 @@ def test_no_duplicate_ips():
 
 def test_aidentry_captures_all_attributes():
     config = load_config()
-    # get a single entry as an example.  call_sock_api returns a list of dicts
+    # get a single entry as an example.  __call_sock_api returns a list of dicts
     entry = call_sock_api(config, 'aggressive_ips')['aggressive_ips'][0]
     # compare the entries keys to the fields in AIDEntry
     assert set(entry.keys()) == set(AIDEntry._fields)
