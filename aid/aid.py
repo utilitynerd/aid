69057e23d78471c57f4b840abf487455ca4800c5
Mike Jones <mikejones@berkeley.edu>
2015-12-14 10:47:11 -0800

    add docstrings

--- aid/aid.py
+++ aid/aid.py
@@ -13,7 +13,15 @@ AIDEntry = collections.namedtuple("AidEntry", ["ip", "tags", "dst_port", "last_s
 CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".aid.json")
 
 
-def call_sock_api(config, endpoint, **params):
+def __call_sock_api(config, endpoint, **params):
+    """
+    Call sock api and return object created from returned json
+
+    :param config: instance of SockConfig
+    :param endpoint: api endpoint for request
+    :param params: dict of additional parameters for api request
+    :return: api response as python object
+    """
     params["token"] = config.token
     url = "{host}/api/{endpoint}".format(host=config.host, endpoint=endpoint)
     r = requests.get(url, timeout=5, params=params)
@@ -21,29 +29,42 @@ def call_sock_api(config, endpoint, **params):
     return json.loads(r.text)
 
 
-def load_config(path=CONFIG_PATH):
+def __load_config(path=CONFIG_PATH):
+    """
+    Instantiates a SockConfig object from config file located at path
+
+    :param path: path to config file
+    :return: SockConfig object
+    """
     with open(path) as fd:
         conf = json.load(fd)
         return SockConfig(host=conf['host'], token=conf['token'])
 
 
-# setting services=[] in function definition can cause strange problems.
-#  See: http://docs.python-guide.org/en/latest/writing/gotchas/
 def entries(services=None, start_date="1 week ago", seen_count=10, config=None):
+    """
+    Query aggressive ips api
+
+    :param services: list of services to include in results. default is all services.
+    :param start_date: return aggresive ip entries starting from this date
+    :param seen_count: threshold for # of times an IP has triggered an alerts
+    :param config: SockConfig object
+    :return: list of AIDEntry objects
+    """
     if services is not None:
         services = list(services)
     else:
         services = []
 
     if not config:
-        config = load_config()
+        config = __load_config()
 
     last_seen_ts = dateparser.parse(start_date)
     if not last_seen_ts:
         sys.exit("{} - invalid start date".format(start_date))
 
-    aid_list = call_sock_api(config, 'aggressive_ips', service=",".join(services),
-                             last_seen_ts=last_seen_ts.isoformat(), seen_count=seen_count)['aggressive_ips']
+    aid_list = __call_sock_api(config, 'aggressive_ips', service=",".join(services),
+                               last_seen_ts=last_seen_ts.isoformat(), seen_count=seen_count)['aggressive_ips']
 
     return [AIDEntry(ip=ipaddress.ip_address(entry['ip']),
                      tags=entry['tags'],
@@ -56,5 +77,14 @@ def entries(services=None, start_date="1 week ago", seen_count=10, config=None):
 
 
 def ips(services=None, start_date="1 week ago", seen_count=10, config=None):
+    """
+    Query aggressive ips api, but only return a list of unique IPs
+
+    :param services: list of services to include in results. default is all services.
+    :param start_date: return aggresive ip entries starting from this date
+    :param seen_count: threshold for # of times an IP has triggered an alerts
+    :param config: SockConfig object
+    :return: list of unique IPs
+    """
     aid_list = entries(services, start_date, seen_count, config)
     return sorted({ipaddress.ip_address(entry.ip) for entry in aid_list})
