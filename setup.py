9b7f976fc6ce78b64110992995d4cc2baa21d847
Mike Jones <mikejones@berkeley.edu>
2015-12-14 10:49:14 -0800

    set version to 0.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import  setup, find_packages
 
 setup(
     name='aid',
-    version='0.0.1.dev3',
+    version='0.0.1',
     url='',
     license='Copyright UC Regents',
     author='Mike Jones',
