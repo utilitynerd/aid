d8d1ca5e25fdb19de5e2ecb91414f68ca53cd1c9
Mike Jones <mikejones@berkeley.edu>
2015-12-15 18:01:51 -0800

    fix imports under python 2.7.11

--- aid/__init__.py
+++ aid/__init__.py
@@ -1,1 +1,6 @@
-from aid.aid import entries, ips, AIDEntry
+try:
+    # python 3
+    from aid.aid import entries, ips, AIDEntry
+except ImportError:
+    # python 2
+    from aid import entries, ips, AIDEntry
