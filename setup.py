3a4d9735112ecfc208a0ce61e8d6a3b3e4d54659
Mike Jones <mikejones@berkeley.edu>
2015-12-18 15:44:27 -0800

    add additional metadata to setup.py

--- setup.py
+++ setup.py
@@ -8,12 +8,21 @@ if sys.version_info[0] == 2:
 
 setup(
     name='aid',
-    version='0.0.3',
-    url='',
-    license='Copyright UC Regents',
+    version='0.1.0',
+    url='https://github.com/utilitynerd/aid',
+    license='Educational Community License, Version 2.0',
     author='Mike Jones',
     author_email='mikejones@security.berkeley.edu',
     description='library and scripts for interacting with ISP AID list',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Networking :: Firewalls',
+        'Topic :: System :: Systems Administration',
+        'License :: OSI Approved',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ],
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
