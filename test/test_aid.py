60c5d5aae221faf18355603d2ad27d5b763774d9
Mike Jones <mikejones@berkeley.edu>
2016-08-11 09:13:50 -0700

    adding tests for aid module

--- test/test_aid.py
+++ test/test_aid.py
@@ -1,7 +1,15 @@
 from aid.aid import *
 
+import pytest
+
+from requests.exceptions import HTTPError
 
 def test_get_list_of_services():
     service_list = services()
     assert isinstance(service_list, list)
     assert len(service_list) > 0
+
+def test_call_sock_http_error_results_in_HTTPError_exception():
+    config = load_config()
+    with pytest.raises(HTTPError) as e:
+        call_sock_api(config, 'fail')
